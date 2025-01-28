b6ace6a9bca2c0076359010fb4f095a68dfe08d8
Shamsunnabi Taqib <shamsunnabi.taqib@ad.enosisbd.com>
2025-01-28 19:04:31 +0600

    added pipelines

--- Blue/app.py
+++ Blue/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Blue App V1 !"
+    return "Welcome to the Blue App V1!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
