24aef5567a81494259b9044736eba77f3234ea7d
Shamsunnabi Taqib <shamsunnabi.taqib@ad.enosisbd.com>
2025-01-29 10:19:27 +0600

    modify green

--- Green/app.py
+++ Green/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to the Green App V1! "
+    return "Welcome to the Green App V2! "
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
